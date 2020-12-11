a732521e0d2f3d3c28634b984901394b3a9ecf74
slimojay <35162232+slimojay@users.noreply.github.com>
2020-12-11 02:22:21 +0100

    Update authen.php

--- panel/panel/model/authen.php
+++ panel/panel/model/authen.php
@@ -2,6 +2,6 @@
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 session_start();
 include('../controller/lcis_portal.php');
-$app = new LcisPanel('localhost', 'lcis_admin', 'Liteweb@2020', 'lcis_crime');
+$app = new LcisPanel();
 $app->checkIsAuth();
 ?>
