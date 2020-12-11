f83f381bb3a999960136b180529b0ba9aee29028
slimojay <35162232+slimojay@users.noreply.github.com>
2020-12-11 02:23:02 +0100

    Update delete.php

--- panel/panel/model/delete.php
+++ panel/panel/model/delete.php
@@ -1,6 +1,6 @@
 <?php
 include('../controller/lcis_portal.php');
-$app = new LcisPanel('localhost', 'lcis_admin', 'Liteweb@2020', 'lcis_crime');
+$app = new LcisPanel();
 if (isset($_GET['lcis_number'])){
     $lcis_number = $_GET['lcis_number'];
     echo $app->deleteUser($lcis_number);   
