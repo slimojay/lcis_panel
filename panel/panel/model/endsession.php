03a299926db335e16c69ad1cdda419fdff088b64
slimojay <35162232+slimojay@users.noreply.github.com>
2020-12-11 02:23:36 +0100

    Update endsession.php

--- panel/panel/model/endsession.php
+++ panel/panel/model/endsession.php
@@ -2,7 +2,7 @@
 ob_start();
 session_start();
 include('../controller/lcis_portal.php');
-$app = new LcisPanel('localhost', 'lcis_admin', 'Liteweb@2020', 'lcis_crime');
+$app = new LcisPanel();
 $app->endSession();
 ob_flush();
 ?>
