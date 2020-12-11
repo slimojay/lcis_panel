fbb7f592cdb47397d024a02e5db22e962959afdd
slimojay <35162232+slimojay@users.noreply.github.com>
2020-12-11 02:21:03 +0100

    Update ajax_fetch.php

--- panel/panel/model/ajax_fetch.php
+++ panel/panel/model/ajax_fetch.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 include('../controller/lcis_portal.php');
-$app = new LcisPanel('localhost', 'lcis_admin', 'Liteweb@2020', 'lcis_crime');
+$app = new LcisPanel();
 if(isset($_GET['lcis_number'])){
     $lcis_number = $app->con->real_escape_string($_GET['lcis_number']);
     echo $app->fetchInmateRecord($lcis_number);
